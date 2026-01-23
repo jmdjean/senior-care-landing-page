fc64a89ca1eaaa577a9dc586880c09ae4afdef44
jmdjean <jmd_jean_12@hotmail.com>
2026-01-23 12:21:42 -0300

    ajuste

--- src/template/js/main.js
+++ src/template/js/main.js
@@ -146,17 +146,20 @@
                 const sectionHeight = current.offsetHeight;
                 const sectionTop = current.offsetTop - 50;
                 const sectionId = current.getAttribute('id');
-            
-               /* If our current scroll position enters the space where current section 
-                * on screen is, add .current class to parent element(li) of the thecorresponding 
-                * navigation link, else remove it. To know which link is active, we use 
-                * sectionId variable we are getting while looping through sections as 
+
+               /* If our current scroll position enters the space where current section
+                * on screen is, add .current class to parent element(li) of the thecorresponding
+                * navigation link, else remove it. To know which link is active, we use
+                * sectionId variable we are getting while looping through sections as
                 * an selector
                 */
+                const navLink = document.querySelector('.s-header__nav a[href*=' + sectionId + ']');
+                if (!navLink) return;
+
                 if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.add('current');
+                    navLink.parentNode.classList.add('current');
                 } else {
-                    document.querySelector('.s-header__nav a[href*=' + sectionId + ']').parentNode.classList.remove('current');
+                    navLink.parentNode.classList.remove('current');
                 }
             });
         }
